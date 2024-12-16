bf7c71367d0633203da3b1133edfc89cdb4f4ca5
philip <pbergman@live.nl>
2024-12-16 14:22:42 +0100

    update to work with libdns

---
+++ api_status.go
@@ -0,0 +1,6 @@
+package mijn_host
+
+type status struct {
+	Code        int    `json:"status"`
+	Description string `json:"status_description"`
+}
--- provider.go
+++ provider.go
@@ -1,380 +1,123 @@
 package mijn_host
 
 import (
-	"bytes"
 	"context"
-	"crypto/sha1"
-	"encoding/hex"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
 	"net"
-	"net/http"
-	"net/url"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/libdns/libdns"
 )
 
 func NewProvider() *Provider {
 	return &Provider{
-		client: &http.Client{},
+		client: NewApiClient("", nil),
 	}
 }
 
-type status struct {
-	Code        int    `json:"status"`
-	Description string `json:"status_description"`
-}
-
 type Provider struct {
-	ApiKey   string `json:"api_key"`
-	client   *http.Client
+	client *ApiClient
+
 	mutex    sync.RWMutex
 	resolver *net.Resolver
 }
 
-func (p *Provider) toPath(zone string) string {
-	return fmt.Sprintf("https://mijn.host/api/v2/domains/%s/dns", url.PathEscape(strings.TrimSuffix(zone, ".")))
+func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
+	p.mutex.RLock()
+	defer p.mutex.RUnlock()
+
+	return p.client.GetRecords(ctx, zone)
 }
 
-func (p *Provider) newRequest(ctx context.Context, method string, zone string, body io.Reader) (*http.Request, error) {
-
-	request, err := http.NewRequestWithContext(ctx, method, p.toPath(zone), body)
-
-	if err != nil {
-		return nil, err
-	}
-
-	request.Header.Set("accept", "application/json")
-	request.Header.Set("content-type", "application/json")
-	request.Header.Set("API-Key", p.ApiKey)
-
-	return request, nil
-}
-
-func (p *Provider) updateRecords(ctx context.Context, zone string, recs []libdns.Record) error {
-
-	type record map[string]interface{}
-
-	var data = map[string][]record{
-		"records": make([]record, len(recs)),
-	}
-
-	for i, c := 0, len(recs); i < c; i++ {
-		data["records"][i] = record{
-			"type":  recs[i].Type,
-			"name":  recs[i].Name,
-			"value": recs[i].Value,
-			"ttl":   recs[i].TTL.Seconds(),
-		}
-	}
-
-	out, err := json.Marshal(data)
-
-	if err != nil {
-		return err
-	}
-
-	request, err := p.newRequest(ctx, "PUT", zone, bytes.NewReader(out))
-
-	if err != nil {
-		return err
-	}
-
-	response, err := p.client.Do(request)
-
-	if err != nil {
-		return err
-	}
-
-	defer response.Body.Close()
-
-	var object status
-
-	if err := json.NewDecoder(response.Body).Decode(&object); err != nil {
-		return err
-	}
-
-	if 200 != object.Code {
-		return errors.New(object.Description)
-	}
-
-	return nil
-}
-
-func (p *Provider) DeleteRecords(ctx context.Context, zone string, recs []libdns.Record) ([]libdns.Record, error) {
-	records, err := p.GetRecords(ctx, zone)
-
-	if err != nil {
-		return nil, err
-	}
-
+func (p *Provider) AppendRecords(ctx context.Context, zone string, recs []libdns.Record) ([]libdns.Record, error) {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-	var removed []libdns.Record
+	existing, err := p.client.GetRecords(ctx, zone)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := p.client.SetRecords(ctx, zone, append(existing, recs...)); err != nil {
+		return nil, err
+	}
+
+	return recs, nil
+}
+
+func (p *Provider) DeleteRecords(ctx context.Context, zone string, recs []libdns.Record) ([]libdns.Record, error) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	existing, err := p.client.GetRecords(ctx, zone)
+
+	if err != nil {
+		return nil, err
+	}
 
 outerLoop:
 
 	for _, remove := range recs {
 		if remove.ID != "" {
-			for idx, x := range records {
+			for idx, x := range existing {
 				if x.ID == remove.ID {
-					records = append(records[:idx], records[idx+1:]...)
-					removed = append(removed, records[idx])
+					existing = append(existing[:idx], existing[idx+1:]...)
 					continue outerLoop
 				}
 			}
 		}
-		for idx, x := range records {
+
+		for idx, x := range existing {
 			if x.Type == remove.Type && x.Name == remove.Name && x.Value == remove.Value && x.TTL == remove.TTL {
-				records = append(records[:idx], records[idx+1:]...)
-				removed = append(removed, records[idx])
+				existing = append(existing[:idx], existing[idx+1:]...)
 				continue outerLoop
 			}
 		}
 	}
 
-	if err := p.updateRecords(ctx, zone, records); err != nil {
+	if err := p.client.SetRecords(ctx, zone, append(existing, recs...)); err != nil {
 		return nil, err
 	}
 
-	return removed, nil
+	return recs, nil
+}
+
+func (p *Provider) equals(a, b *libdns.Record) bool {
+	return (a.Type == "CNAME" && b.Type == "CNAME" && a.Name == b.Name) || (a.ID != "" && a.ID == b.ID)
 }
 
 func (p *Provider) SetRecords(ctx context.Context, zone string, recs []libdns.Record) ([]libdns.Record, error) {
-	records, err := p.GetRecords(ctx, zone)
-
-	if err != nil {
-		return nil, err
-	}
-
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-next:
-	for i, c := 0, len(recs); i < c; i++ {
+	existing, err := p.client.GetRecords(ctx, zone)
 
-		if recs[i].ID != "" {
-			for x, y := 0, len(records); x < y; x++ {
-				if records[x].ID == recs[i].ID {
-					records[x].Name = recs[i].Name
-					records[x].Type = recs[i].Type
-					records[x].TTL = recs[i].TTL
-					records[x].Value = recs[i].Value
-				}
-				continue next
-			}
-		}
-
-		if recs[i].Type == "CNAME" {
-			for x, y := 0, len(records); x < y; x++ {
-				if records[x].Type == "CNAME" && records[x].Name == recs[i].Name {
-					records[x].Name = recs[i].Name
-					records[x].Type = recs[i].Type
-					records[x].TTL = recs[i].TTL
-					records[x].Value = recs[i].Value
-				}
-				continue next
-			}
-		}
-
-		records = append(records, recs[i])
-	}
-
-	if err := p.updateRecords(ctx, zone, records); err != nil {
+	if err != nil {
 		return nil, err
 	}
 
+outerLoop:
+
 	for i, c := 0, len(recs); i < c; i++ {
-		recs[i].ID = p.generateId(&recs[i])
+		for x, y := 0, len(existing); x < y; x++ {
+			if p.equals(&recs[i], &existing[x]) {
+				existing[x] = recs[i]
+				continue outerLoop
+			}
+		}
+
+		if "" == recs[i].ID {
+			recs[i].ID = p.client.makeId(&recs[i])
+		}
+
+		existing = append(existing, recs[i])
+	}
+
+	if err := p.client.SetRecords(ctx, zone, existing); err != nil {
+		return nil, err
 	}
 
 	return recs, nil
-	//return p.validate(ctx, recs), nil
-}
-
-func (p *Provider) AppendRecords(ctx context.Context, zone string, recs []libdns.Record) ([]libdns.Record, error) {
-	records, err := p.GetRecords(ctx, zone)
-
-	if err != nil {
-		return nil, err
-	}
-
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
-
-	for i, c := 0, len(recs); i < c; i++ {
-		records = append(records, recs[i])
-	}
-
-	if err := p.updateRecords(ctx, zone, records); err != nil {
-		return nil, err
-	}
-
-	for i, c := 0, len(recs); i < c; i++ {
-		recs[i].ID = p.generateId(&recs[i])
-	}
-
-	return recs, nil
-	//return p.validate(ctx, recs), nil
-}
-
-func (p *Provider) validate(ctx context.Context, recs []libdns.Record) []libdns.Record {
-
-	p.sleep(ctx, 200*time.Millisecond)
-
-	if nil == p.resolver {
-		p.resolver = &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				d := net.Dialer{
-					Timeout: time.Millisecond * time.Duration(10000),
-				}
-
-				return d.DialContext(ctx, network, "208.67.222.222:53")
-			},
-		}
-	}
-
-	var success []libdns.Record
-
-loop:
-	for i, c := 0, len(recs); i < c; i++ {
-
-		switch recs[i].Type {
-		case "A", "AAAA":
-			var network = map[string]string{
-				"A":    "ipv4",
-				"AAAA": "ipv6",
-			}
-			for n, r := 0, 5; n < r; n++ {
-				list, err := p.resolver.LookupIP(ctx, network[recs[i].Type], recs[i].Name)
-				if err == nil {
-					for _, item := range list {
-						if item.String() == recs[i].Value {
-							success = append(success, recs[i])
-							continue loop
-						}
-					}
-				}
-				p.sleep(ctx, 500*time.Millisecond)
-			}
-		case "CNAME":
-			for n, r := 0, 5; n < r; n++ {
-				item, err := p.resolver.LookupCNAME(ctx, recs[i].Name)
-				if err == nil {
-					if item == recs[i].Value {
-						success = append(success, recs[i])
-						continue loop
-					}
-				}
-				p.sleep(ctx, 500*time.Millisecond)
-			}
-			break
-		case "NS":
-			for n, r := 0, 5; n < r; n++ {
-				list, err := p.resolver.LookupNS(ctx, recs[i].Name)
-				if err == nil {
-					for _, item := range list {
-						if item.Host == recs[i].Value {
-							success = append(success, recs[i])
-							continue loop
-						}
-					}
-				}
-				p.sleep(ctx, 500*time.Millisecond)
-			}
-			break
-		case "TXT":
-			for n, r := 0, 5; n < r; n++ {
-				list, err := p.resolver.LookupTXT(ctx, recs[i].Name)
-				if err == nil {
-					for _, item := range list {
-						if item == recs[i].Value {
-							success = append(success, recs[i])
-							continue loop
-						}
-					}
-				}
-				p.sleep(ctx, 500*time.Millisecond)
-			}
-			break
-		default:
-			// @todo validate other record types [ALIAS, CAA, DS, MX, SPF, SRV, TLSA]
-			success = append(success, recs[i])
-		}
-
-	}
-
-	return success
-}
-func (p *Provider) sleep(ctx context.Context, delay time.Duration) {
-	var timer = time.NewTimer(delay)
-	select {
-	case <-ctx.Done():
-		if !timer.Stop() {
-			<-timer.C
-		}
-	case <-timer.C:
-	}
-}
-
-func (p *Provider) generateId(record *libdns.Record) string {
-	var hash = sha1.New()
-	_, _ = fmt.Fprintf(hash, "[type:%s]", record.Type)
-	_, _ = fmt.Fprintf(hash, "[name:%s]", record.Name)
-	_, _ = fmt.Fprintf(hash, "[value:%s]", record.Value)
-	_, _ = fmt.Fprintf(hash, "[ttl:%s]", record.TTL)
-
-	return hex.EncodeToString(hash.Sum(nil))
-}
-
-func (p *Provider) GetRecords(ctx context.Context, zone string) ([]libdns.Record, error) {
-
-	p.mutex.RLock()
-	defer p.mutex.RUnlock()
-
-	request, err := p.newRequest(ctx, "GET", zone, nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	response, err := p.client.Do(request)
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer response.Body.Close()
-
-	var object = struct {
-		status
-		Data struct {
-			Domain  string
-			Records *[]libdns.Record
-		}
-	}{}
-
-	if err := json.NewDecoder(response.Body).Decode(&object); err != nil {
-		return nil, err
-	}
-
-	if 200 != object.status.Code {
-		return nil, errors.New(object.status.Description)
-	}
-
-	for i, c := 0, len(*object.Data.Records); i < c; i++ {
-		(*object.Data.Records)[i].ID = p.generateId(&(*object.Data.Records)[i])
-		(*object.Data.Records)[i].TTL *= time.Second
-	}
-
-	return *object.Data.Records, nil
 }
 
 // Interface guards
